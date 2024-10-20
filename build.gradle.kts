2be2f0c1e5ea2baa6dce65897b29e25ef1dcbb72
Sergey Korshunoff <seyko2@gmail.com>
2024-10-20 08:20:42 +0300

    "Hello, World!" in Kotlin. Compile by ./gradlew build, result in build/disrtibution To make jar file in build/lib executable by java unzip it and "jar -e org.example.HelloKt -c -f Hello-0.1.jar META-INF org"

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+    application
+}
+
+group = "org.example"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("org.example.HelloKt")
+}

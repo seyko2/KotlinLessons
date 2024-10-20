e6ebdb5b4a17f41892e9103f6a507e76f3f672f9
Sergey Korshunoff <seyko2@gmail.com>
2024-10-20 11:14:26 +0300

    More complex ptintln() statemets. Now you can't compile Hello.kt by kotlinc, only by grandlew support

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -8,14 +8,14 @@ fun main() {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-//    println("Hello, " + name + "!")
+    println("Hello, " + name + "!")
     println("Hello, $name!")
     println("Hello, World!")
 
-//    for (i in 1..5) {
-//        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-//        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-//        println("i = $i")
-//    }
+    for (i in 1..5) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        println("i = $i")
+    }
 
 }
